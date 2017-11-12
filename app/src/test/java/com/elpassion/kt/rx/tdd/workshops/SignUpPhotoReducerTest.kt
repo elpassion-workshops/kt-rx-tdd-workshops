905f38942b052283e6b99b7d56b00a7c277f403e
ghostbuster91 <kghost0@gmail.com>
2017-11-12 21:11:16 +0100

    Not have photo when permission denied

--- app/src/test/java/com/elpassion/kt/rx/tdd/workshops/SignUpPhotoReducerTest.kt
+++ app/src/test/java/com/elpassion/kt/rx/tdd/workshops/SignUpPhotoReducerTest.kt
@@ -29,10 +29,19 @@ class SignUpPhotoReducerTest {
         state.assertLastValue(SignUp.Photo.State.Photo("photo uri"))
     }
 
+    @Test
+    fun shouldNotHavePhotoWhenPermissionDenied() {
+        events.accept(SignUp.Photo.TakePhotoEvent)
+        state.assertLastValue(SignUp.Photo.State.EMPTY)
+    }
+
     class PhotoReducer(private val permissionRequester: SignUp.Photo.PermissionRequester,
                        private val photoRequester: SignUp.Photo.PhotoRequester) : (Observable<Any>) -> Observable<SignUp.Photo.State> {
         override fun invoke(events: Observable<Any>): Observable<SignUp.Photo.State> {
             return events.ofType(SignUp.Photo.TakePhotoEvent::class.java)
+                    .switchMap {
+                        permissionRequester.request().toObservable()
+                    }
                     .map<SignUp.Photo.State> { SignUp.Photo.State.Photo("photo uri") }
                     .startWith(SignUp.Photo.State.EMPTY)
         }
