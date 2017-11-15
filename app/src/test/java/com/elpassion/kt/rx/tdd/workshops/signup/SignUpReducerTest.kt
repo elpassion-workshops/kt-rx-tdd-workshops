f1559979e01931bde5c6300bae9b97e3dce27a34
ghostbuster91 <kghost0@gmail.com>
2017-11-15 13:36:41 +0100

    LoginValidation state is idle at the beginning

--- app/src/test/java/com/elpassion/kt/rx/tdd/workshops/signup/SignUpReducerTest.kt
+++ app/src/test/java/com/elpassion/kt/rx/tdd/workshops/signup/SignUpReducerTest.kt
@@ -1,12 +1,27 @@
 package com.elpassion.kt.rx.tdd.workshops.signup
 
-import org.junit.Assert
+import com.elpassion.kt.rx.tdd.workshops.assertLastValueThat
+import com.jakewharton.rxrelay2.BehaviorRelay
 import org.junit.Test
 
 class SignUpReducerTest {
 
+    private val state = BehaviorRelay.createDefault(SignUp.State(SignUp.LoginValidation.State.IDLE)).test()
+
     @Test
-    fun shouldPass() {
-        Assert.assertEquals(4, 2 + 2)
+    fun shouldLoginValidationStateBeIdleAtTheBegging() {
+        state.assertLastValueThat { loginValidation == SignUp.LoginValidation.State.IDLE }
+    }
+
+}
+
+interface SignUp {
+    data class State(val loginValidation: LoginValidation.State)
+
+    interface LoginValidation {
+        enum class State {
+            IDLE,
+        }
     }
 }
+
