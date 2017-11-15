60e4babe6407b711e9be7e0b0f5e605b36a55b78
ghostbuster91 <kghost0@gmail.com>
2017-11-15 13:45:49 +0100

    shouldLoginValidationStateBeInProgressWhenNotEmptyLoginArrives

--- app/src/test/java/com/elpassion/kt/rx/tdd/workshops/signup/SignUpReducerTest.kt
+++ app/src/test/java/com/elpassion/kt/rx/tdd/workshops/signup/SignUpReducerTest.kt
@@ -1,18 +1,34 @@
 package com.elpassion.kt.rx.tdd.workshops.signup
 
 import com.elpassion.kt.rx.tdd.workshops.assertLastValueThat
-import com.jakewharton.rxrelay2.BehaviorRelay
+import com.elpassion.kt.rx.tdd.workshops.common.Events
+import com.elpassion.kt.rx.tdd.workshops.common.Reducer
+import com.jakewharton.rxrelay2.PublishRelay
+import io.reactivex.Observable
 import org.junit.Test
 
 class SignUpReducerTest {
 
-    private val state = BehaviorRelay.createDefault(SignUp.State(SignUp.LoginValidation.State.IDLE)).test()
+    private val events = PublishRelay.create<Any>()
+    private val state = SignUpReducer().invoke(events).test()
 
     @Test
     fun shouldLoginValidationStateBeIdleAtTheBegging() {
         state.assertLastValueThat { loginValidation == SignUp.LoginValidation.State.IDLE }
     }
 
+    @Test
+    fun shouldLoginValidationStateBeInProgressWhenNotEmptyLoginArrives() {
+        events.accept(SignUp.LoginValidation.LoginChangedEvent("login"))
+        state.assertLastValueThat { loginValidation == SignUp.LoginValidation.State.LOADING }
+    }
+}
+
+class SignUpReducer : Reducer<SignUp.State> {
+    override fun invoke(events: Events): Observable<SignUp.State> {
+        return events.map { SignUp.State(SignUp.LoginValidation.State.LOADING) }
+                .startWith(SignUp.State(SignUp.LoginValidation.State.IDLE))
+    }
 }
 
 interface SignUp {
@@ -21,7 +37,10 @@ interface SignUp {
     interface LoginValidation {
         enum class State {
             IDLE,
+            LOADING,
         }
+
+        data class LoginChangedEvent(val login: String)
     }
 }
 
