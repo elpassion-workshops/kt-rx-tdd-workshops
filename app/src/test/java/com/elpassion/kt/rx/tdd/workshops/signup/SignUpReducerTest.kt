1aa80acdc78d062db435b398c4589d6e3cafcde9
jozzi <dominik.malantowicz@gmail.com>
2017-11-16 18:28:15 +0100

    Change login validation state to loading after login change

--- app/src/test/java/com/elpassion/kt/rx/tdd/workshops/signup/SignUpReducerTest.kt
+++ app/src/test/java/com/elpassion/kt/rx/tdd/workshops/signup/SignUpReducerTest.kt
@@ -1,27 +1,46 @@
 package com.elpassion.kt.rx.tdd.workshops.signup
 
 import com.elpassion.kt.rx.tdd.workshops.assertLastValueThat
+import com.elpassion.kt.rx.tdd.workshops.common.Events
+import com.elpassion.kt.rx.tdd.workshops.common.Reducer
+import com.jakewharton.rxrelay2.PublishRelay
 import io.reactivex.Observable
-import io.reactivex.observers.TestObserver
 import org.junit.Test
 
 class SignUpReducerTest {
 
-    private val state = Observable.just(SignUp.State(SignUp.LoginValidation.State.IDLE)).test()
+    private val events = PublishRelay.create<Any>()
+    private val state = SignUpRelay().invoke(events).test()
 
     @Test
     fun shouldLoginValidationStateBeIdleAtTheBegging() {
         state.assertLastValueThat { loginValidation == SignUp.LoginValidation.State.IDLE }
     }
 
+    @Test
+    fun shouldChangeLoginValidationStateToLoadingAfterChangingLogin() {
+        events.accept(SignUp.LoginValidation.LoginChangedEvent("a"))
+        state.assertLastValueThat { loginValidation == SignUp.LoginValidation.State.LOADING }
+    }
 }
 
 interface SignUp {
     data class State(val loginValidation: LoginValidation.State)
 
     interface LoginValidation {
+        data class LoginChangedEvent(val login: String)
+
         enum class State {
             IDLE,
+            LOADING,
         }
+    }
+}
+
+class SignUpRelay : Reducer<SignUp.State> {
+    override fun invoke(events: Events): Observable<SignUp.State> {
+        return events.map { SignUp.LoginValidation.State.LOADING }
+                .startWith(SignUp.LoginValidation.State.IDLE)
+                .map { SignUp.State(it) }
     }
 }
