8c7d3cc28cde29a8dc8d0603af361e86182b3e5c
jozzi <dominik.malantowicz@gmail.com>
2017-11-16 18:07:19 +0100

    Create extension method for hasBitmap matcher

--- app/src/androidTest/java/com/elpassion/kt/rx/tdd/workshops/HasSameBitmap.kt
+++ app/src/androidTest/java/com/elpassion/kt/rx/tdd/workshops/HasSameBitmap.kt
@@ -2,17 +2,21 @@ package com.elpassion.kt.rx.tdd.workshops
 
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
+import android.support.test.espresso.ViewInteraction
+import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.BoundedMatcher
 import android.view.View
 import android.widget.ImageView
 import org.hamcrest.Description
 
-class HasSameBitmap(private val expectedBitmap: Bitmap) : BoundedMatcher<View, ImageView>(ImageView::class.java) {
+fun ViewInteraction.hasBitmap(expectedBitmap: Bitmap) =
+        check(matches(hasBitmapMatcher(expectedBitmap)))
+
+private fun hasBitmapMatcher(expectedBitmap: Bitmap) = object : BoundedMatcher<View, ImageView>(ImageView::class.java) {
     override fun describeTo(description: Description) {
         description.appendText("has bitmap does not match expected bitmap")
     }
 
-    override fun matchesSafely(item: ImageView): Boolean {
-        return (item.drawable as? BitmapDrawable)?.bitmap?.sameAs(expectedBitmap) ?: false
-    }
+    override fun matchesSafely(item: ImageView) =
+            (item.drawable as? BitmapDrawable)?.bitmap?.sameAs(expectedBitmap) ?: false
 }
