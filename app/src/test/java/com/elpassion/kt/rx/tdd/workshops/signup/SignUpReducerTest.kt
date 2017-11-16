baf30c201e67f591947daa997962dd7068fc25bf
jozzi <dominik.malantowicz@gmail.com>
2017-11-16 18:18:07 +0100

    Login validation state be idle at the begging

--- app/src/test/java/com/elpassion/kt/rx/tdd/workshops/signup/SignUpReducerTest.kt
+++ app/src/test/java/com/elpassion/kt/rx/tdd/workshops/signup/SignUpReducerTest.kt
@@ -1,12 +1,27 @@
 package com.elpassion.kt.rx.tdd.workshops.signup
 
-import org.junit.Assert
+import com.elpassion.kt.rx.tdd.workshops.assertLastValueThat
+import io.reactivex.Observable
+import io.reactivex.observers.TestObserver
 import org.junit.Test
 
 class SignUpReducerTest {
 
+    private val state = Observable.just(SignUp.State(SignUp.LoginValidation.State.IDLE)).test()
+
     @Test
-    fun shouldPass() {
-        Assert.assertEquals(4, 2 + 2)
+    fun shouldLoginValidationStateBeIdleAtTheBegging() {
+        state.assertLastValueThat { loginValidation == SignUp.LoginValidation.State.IDLE }
+    }
+
+}
+
+interface SignUp {
+    data class State(val loginValidation: LoginValidation.State)
+
+    interface LoginValidation {
+        enum class State {
+            IDLE,
+        }
     }
 }
