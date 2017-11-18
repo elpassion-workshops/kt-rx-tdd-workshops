f04c3c08dcb316f661df22caf6ac8808fc24aa8c
Michal Cwikinski <michal@cwiklinski.mobi>
2017-11-18 16:46:48 +0100

    production implementation

--- app/src/main/java/com/elpassion/kt/rx/tdd/workshops/MyFirstApp.kt
+++ app/src/main/java/com/elpassion/kt/rx/tdd/workshops/MyFirstApp.kt
@@ -2,10 +2,19 @@ package com.elpassion.kt.rx.tdd.workshops
 
 import android.app.Application
 import com.elpassion.kt.rx.tdd.workshops.common.CurrentActivityProvider
+import com.elpassion.kt.rx.tdd.workshops.common.requestCameraPermission
+import com.elpassion.kt.rx.tdd.workshops.common.requestPhoto
+import com.elpassion.kt.rx.tdd.workshops.signup.SignUp
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
 
 class MyFirstApp : Application() {
     override fun onCreate() {
         super.onCreate()
         registerActivityLifecycleCallbacks(CurrentActivityProvider)
+        SignUp.debounceScheduler = AndroidSchedulers.mainThread()
+        SignUp.api = { Single.just(true) }
+        SignUp.camera = { requestPhoto() }
+        SignUp.permissions = { requestCameraPermission() }
     }
 }
