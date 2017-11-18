661e655dfdc945526cf9db46aeef0fcce0f36ef8
Bartosz Kraszewski <bkraszewski@gmail.com>
2017-11-18 14:39:53 +0100

    shouldShowPhotoFromCameraAfterTakingPhotoAndPermissionsGranted

--- app/src/test/java/com/elpassion/kt/rx/tdd/workshops/signup/SignUpReducerTest.kt
+++ app/src/test/java/com/elpassion/kt/rx/tdd/workshops/signup/SignUpReducerTest.kt
@@ -8,10 +8,9 @@ import com.jakewharton.rxrelay2.PublishRelay
 import com.nhaarman.mockito_kotlin.*
 import io.reactivex.Observable
 import io.reactivex.Single
-import io.reactivex.functions.BiConsumer
+import io.reactivex.rxkotlin.Observables
 import io.reactivex.subjects.SingleSubject
 import org.junit.Test
-import org.reactivestreams.Subscriber
 
 class SignUpReducerTest {
 
@@ -19,14 +18,20 @@ class SignUpReducerTest {
     private val api = mock<(String) -> Single<Boolean>>().apply {
         whenever(this.invoke(any())).thenReturn(apiSubject)
     }
-    private val cameraMock = mock<() -> Single<String>>()
+    private val cameraSubject = SingleSubject.create<String>()
+    private val cameraApi = mock<(() -> Single<String>)>().apply {
+        whenever(this.invoke()).thenReturn(cameraSubject)
+    }
+
     private val events = PublishRelay.create<Any>()
 
     private val permissionSubject = SingleSubject.create<Boolean>()
     private val permission = mock<(() -> Single<Boolean>)>().apply {
         whenever(this.invoke()).thenReturn(permissionSubject);
     }
-    private val state = SignUpReducer(api, cameraMock, permission).invoke(events).test()
+
+
+    private val state = SignUpReducer(api, cameraApi, permission).invoke(events).test()
 
     @Test
     fun shouldLoginValidationStateBeIdleOnStart() {
@@ -77,37 +82,49 @@ class SignUpReducerTest {
 
     @Test
     fun shouldPhotoStateBeEmptyAtTheBegging() {
-        state.assertLastValueThat { photoState == Photo.State.EMPTY }
+        state.assertLastValueThat { photoState == Photo.State.Empty }
     }
 
     @Test
-    fun shouldCallCameraWhenTakingPhoto(){
+    fun shouldCallCameraWhenTakingPhoto() {
         events.accept(Photo.TakePhotoEvent)
         permissionSubject.onSuccess(true)
-        verify(cameraMock).invoke()
+        verify(cameraApi).invoke()
     }
 
     @Test
     fun shouldNotCallCameraWithoutPermissionsWhenTakingPhoto() {
         events.accept(Photo.TakePhotoEvent)
         permissionSubject.onSuccess(false)
-        verify(cameraMock, never()).invoke()
+        verify(cameraApi, never()).invoke()
+    }
+
+    @Test
+    fun shouldShowPhotoAfterTakingPhotoAndPermissionsGranted() {
+        events.accept(Photo.TakePhotoEvent)
+        permissionSubject.onSuccess(true)
+        cameraSubject.onSuccess("uri")
+        state.assertLastValueThat { photoState == Photo.State.Captured("uri") }
     }
 }
 
 class SignUpReducer(private val loginValidationApi: (String) -> Single<Boolean>,
                     private val cameraApi: () -> Single<String>,
-                    private val permission: () -> Single<Boolean>) : Reducer<SignUp.State> {
+                    private val permission: () -> Single<Boolean>
+                    ) : Reducer<SignUp.State> {
     override fun invoke(events: Events): Observable<SignUp.State> {
-        permission().subscribe { hasPermission ->
-                if(hasPermission) {
-                    cameraApi.invoke()
-                }
-        }
-
-        return loginValidationReducer(events)
-                .map {State(it)}
+        return Observables.combineLatest(loginValidationReducer(events), photoReducer(), SignUp::State)
     }
+
+    private fun photoReducer(): Observable<Photo.State> =
+            permission()
+                    .filter { it }
+                    .flatMapSingle {
+                        cameraApi
+                                .invoke()
+                                .map<Photo.State> (Photo.State::Captured )
+                    }.toObservable()
+                    .startWith(Photo.State.Empty)
 
     private fun loginValidationReducer(events: Events): Observable<LoginValidation.State> {
         return events
@@ -136,7 +153,7 @@ class SignUpReducer(private val loginValidationApi: (String) -> Single<Boolean>,
 }
 
 interface SignUp {
-    data class State(val loginValidation: LoginValidation.State, val photoState: Photo.State = Photo.State.EMPTY)
+    data class State(val loginValidation: LoginValidation.State, val photoState: Photo.State)
 
     interface LoginValidation {
         data class LoginChangedEvent(val login: String)
@@ -150,13 +167,13 @@ interface SignUp {
         }
     }
 
-    interface Photo{
-        enum class State{
-            EMPTY,
+    interface Photo {
+        sealed class State {
+            object Empty : State()
+            data class Captured(val photoUri: String) : State()
         }
 
         object TakePhotoEvent {
         }
-
     }
 }
