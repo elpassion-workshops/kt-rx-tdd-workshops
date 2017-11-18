c3543fd9136932e2c473dea08f344054536f5170
Michal Cwikinski <michal@cwiklinski.mobi>
2017-11-18 11:25:52 +0100

    shouldLoginValidationStateBeAvailableWhenApiPasses

--- app/src/test/java/com/elpassion/kt/rx/tdd/workshops/signup/SignUpReducerTest.kt
+++ app/src/test/java/com/elpassion/kt/rx/tdd/workshops/signup/SignUpReducerTest.kt
@@ -6,12 +6,15 @@ import com.elpassion.kt.rx.tdd.workshops.common.Reducer
 import com.elpassion.kt.rx.tdd.workshops.signup.SignUp.*
 import com.jakewharton.rxrelay2.PublishRelay
 import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.subjects.SingleSubject
 import org.junit.Test
 
 class SignUpReducerTest {
 
     private val events = PublishRelay.create<Any>()
-    private val state = SignUpReducer().invoke(events).test()
+    private val state = SignUpReducer({ apiSubject }).invoke(events).test()
+    private val apiSubject = SingleSubject.create<Boolean>()
 
     @Test
     fun shouldLoginValidationStateBeIdleOnStart() {
@@ -30,17 +33,39 @@ class SignUpReducerTest {
         events.accept(LoginValidation.LoginChangedEvent(""))
         state.assertLastValueThat { loginValidation == LoginValidation.State.IDLE }
     }
+
+    @Test
+    fun shouldLoginValidationStateBeAvailableWhenApiPasses() {
+        val login = "a"
+        events.accept(LoginValidation.LoginChangedEvent(login))
+        apiSubject.onSuccess(true)
+        state.assertLastValueThat { loginValidation == LoginValidation.State.AVAILABLE }
+    }
+
+    @Test
+    fun shouldLoginValidationStateBeTakenWhenApiFails() {
+        val login = "a"
+        events.accept(LoginValidation.LoginChangedEvent(login))
+        apiSubject.onSuccess(false)
+        state.assertLastValueThat { loginValidation == LoginValidation.State.TAKEN }
+    }
 }
 
-class SignUpReducer : Reducer<SignUp.State> {
+class SignUpReducer(val api: () -> Single<Boolean>) : Reducer<SignUp.State> {
     override fun invoke(events: Events): Observable<SignUp.State> {
         return events
                 .ofType(LoginValidation.LoginChangedEvent::class.java)
-                .map {
-                    if (it.login.isEmpty())
-                        LoginValidation.State.IDLE
-                    else
-                        LoginValidation.State.IN_PROGRESS
+                .switchMap {
+                    if (it.login.isEmpty()) {
+                        Observable.just(LoginValidation.State.IDLE)
+                    } else {
+                        api.invoke()
+                                .map {
+                                    if (it) LoginValidation.State.AVAILABLE else LoginValidation.State.TAKEN
+                                }
+                                .toObservable()
+                                .startWith(LoginValidation.State.IN_PROGRESS)
+                    }
                 }
                 .startWith(LoginValidation.State.IDLE)
                 .map(SignUp::State)
@@ -56,6 +81,8 @@ interface SignUp {
         enum class State {
             IDLE,
             IN_PROGRESS,
+            AVAILABLE,
+            TAKEN
         }
     }
 }
