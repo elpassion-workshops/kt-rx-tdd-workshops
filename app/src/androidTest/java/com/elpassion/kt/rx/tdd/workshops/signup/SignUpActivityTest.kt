80f9b21990ff8f86001247f2daf47914c96d2ccc
Antoni Moroz <antoni.moroz@javeo.eu>
2017-11-18 15:17:24 +0100

    Simple login text espresso test

--- app/src/androidTest/java/com/elpassion/kt/rx/tdd/workshops/signup/SignUpActivityTest.kt
+++ app/src/androidTest/java/com/elpassion/kt/rx/tdd/workshops/signup/SignUpActivityTest.kt
@@ -1,6 +1,10 @@
 package com.elpassion.kt.rx.tdd.workshops.signup
 
 import android.support.test.rule.ActivityTestRule
+import com.elpassion.android.commons.espresso.hasText
+import com.elpassion.android.commons.espresso.onId
+import com.elpassion.android.commons.espresso.typeText
+import com.elpassion.kt.rx.tdd.workshops.R
 import org.junit.Rule
 import org.junit.Test
 
@@ -13,4 +17,13 @@ class SignUpActivityTest {
     @Test
     fun shouldStartActivity() {
     }
+
+    @Test
+    fun shouldHaveLoginInput() {
+        onId(R.id.login_input)
+                .typeText("login")
+                .hasText("login")
+    }
+
+
 }
