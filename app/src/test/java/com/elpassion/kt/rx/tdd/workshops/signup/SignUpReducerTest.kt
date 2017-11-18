4a3f74ab61e47c25c79996ec8d9a0719de764ddc
Jogosb <filip.radon@gmail.com>
2017-11-18 11:18:04 +0100

    api validates login

--- app/src/test/java/com/elpassion/kt/rx/tdd/workshops/signup/SignUpReducerTest.kt
+++ app/src/test/java/com/elpassion/kt/rx/tdd/workshops/signup/SignUpReducerTest.kt
@@ -6,12 +6,15 @@ import com.elpassion.kt.rx.tdd.workshops.common.Reducer
 import com.elpassion.kt.rx.tdd.workshops.signup.SignUp.*
 import com.jakewharton.rxrelay2.PublishRelay
 import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.subjects.SingleSubject
 import org.junit.Test
 
 class SignUpReducerTest {
 
     private val events = PublishRelay.create<Any>()
-    private val state = SignUpReducer().invoke(events).test()
+    private val apiSubject = SingleSubject.create<Boolean>()
+    private val state = SignUpReducer({ apiSubject }).invoke(events).test()
 
     @Test
     fun shouldLoginValidationStateBeIdleOnStart() {
@@ -23,18 +26,34 @@ class SignUpReducerTest {
         events.accept(LoginValidation.LoginChangedEvent("a"))
         state.assertLastValueThat { loginValidation == LoginValidation.State.IN_PROGRESS }
     }
+
+    @Test
+    fun shouldLoginValidationStateBeLoginUnavailableAfterUserTypeTakenLogin() {
+        events.accept(LoginValidation.LoginChangedEvent("a"))
+        apiSubject.onSuccess(false)
+        state.assertLastValueThat { loginValidation == LoginValidation.State.LOGIN_TAKEN }
+    }
 }
 
-class SignUpReducer : Reducer<SignUp.State> {
+class SignUpReducer(private val loginApi: () -> Single<Boolean>) : Reducer<SignUp.State> {
     override fun invoke(events: Events): Observable<SignUp.State> {
-        return events.map { LoginValidation.State.IN_PROGRESS }
+        return events
+                .flatMap { callLoginValidationApi() }
                 .startWith(LoginValidation.State.IDLE)
                 .map(SignUp::State)
+    }
+
+    private fun callLoginValidationApi(): Observable<LoginValidation.State> {
+        return loginApi()
+                .map { LoginValidation.State.LOGIN_TAKEN }
+                .toObservable()
+                .startWith(LoginValidation.State.IN_PROGRESS)
     }
 }
 
 interface SignUp {
     data class State(val loginValidation: LoginValidation.State)
+
 
     interface LoginValidation {
         data class LoginChangedEvent(val login: String)
@@ -42,6 +61,7 @@ interface SignUp {
         enum class State {
             IDLE,
             IN_PROGRESS,
+            LOGIN_TAKEN
         }
     }
 }
