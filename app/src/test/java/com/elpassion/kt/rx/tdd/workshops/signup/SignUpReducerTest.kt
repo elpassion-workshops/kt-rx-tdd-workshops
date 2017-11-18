886861f5a50a284fcb88ea27bf7c2da2f5155441
Karol Kowalski <ordon1313@gmail.com>
2017-11-18 09:59:11 +0100

    LoginValidationState is idle on start

--- app/src/test/java/com/elpassion/kt/rx/tdd/workshops/signup/SignUpReducerTest.kt
+++ app/src/test/java/com/elpassion/kt/rx/tdd/workshops/signup/SignUpReducerTest.kt
@@ -1,12 +1,26 @@
 package com.elpassion.kt.rx.tdd.workshops.signup
 
-import org.junit.Assert
+import com.elpassion.kt.rx.tdd.workshops.assertLastValueThat
+import com.elpassion.kt.rx.tdd.workshops.signup.SignUpReducerTest.SignUp.LoginValidation.State
+import io.reactivex.Observable
 import org.junit.Test
 
 class SignUpReducerTest {
 
+    private val state = Observable.just(SignUp.State(State.IDLE)).test()
+
     @Test
-    fun shouldPass() {
-        Assert.assertEquals(4, 2 + 2)
+    fun shouldLoginValidationStateBeIdleOnStart() {
+        state.assertLastValueThat { loginValidation == State.IDLE }
+    }
+
+    interface SignUp {
+        data class State(val loginValidation: LoginValidation.State)
+
+        interface LoginValidation {
+            enum class State {
+                IDLE,
+            }
+        }
     }
 }
