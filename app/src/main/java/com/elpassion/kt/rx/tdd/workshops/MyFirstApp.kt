84a796e3201ebb045d83487e65607b17de313ec1
orbite-mobile <orbitemobile@gmail.com>
2017-11-18 16:50:27 +0100

    Production Dependencies

--- app/src/main/java/com/elpassion/kt/rx/tdd/workshops/MyFirstApp.kt
+++ app/src/main/java/com/elpassion/kt/rx/tdd/workshops/MyFirstApp.kt
@@ -2,10 +2,26 @@ package com.elpassion.kt.rx.tdd.workshops
 
 import android.app.Application
 import com.elpassion.kt.rx.tdd.workshops.common.CurrentActivityProvider
+import com.elpassion.kt.rx.tdd.workshops.common.requestCameraPermission
+import com.elpassion.kt.rx.tdd.workshops.common.requestPhoto
+import com.elpassion.kt.rx.tdd.workshops.signup.SignUp
+import io.reactivex.Single
 
 class MyFirstApp : Application() {
     override fun onCreate() {
         super.onCreate()
         registerActivityLifecycleCallbacks(CurrentActivityProvider)
+
+        SignUp.camera = ::requestPhoto
+        SignUp.cameraPermission = ::requestCameraPermission
+        SignUp.loginApi = object : () -> Single<Boolean> {
+            override fun invoke(): Single<Boolean> {
+                return Single.just(true)
+            }
+
+        }
+
     }
+
+
 }
