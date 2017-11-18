299e43a0088a4bb4ab04b9a944e7e397aa97fe05
Samuel Urbanowicz <samuel.urbanowicz@netguru.pl>
2017-11-18 17:40:51 +0100

    Implement dependencies

--- app/src/main/java/com/elpassion/kt/rx/tdd/workshops/MyFirstApp.kt
+++ app/src/main/java/com/elpassion/kt/rx/tdd/workshops/MyFirstApp.kt
@@ -2,10 +2,35 @@ package com.elpassion.kt.rx.tdd.workshops
 
 import android.app.Application
 import com.elpassion.kt.rx.tdd.workshops.common.CurrentActivityProvider
+import com.elpassion.kt.rx.tdd.workshops.common.requestCameraPermission
+import com.elpassion.kt.rx.tdd.workshops.common.requestPhoto
+import com.elpassion.kt.rx.tdd.workshops.signup.Camera
+import com.elpassion.kt.rx.tdd.workshops.signup.LoginApi
+import com.elpassion.kt.rx.tdd.workshops.signup.SignUpActivity
+import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
+import java.util.*
 
 class MyFirstApp : Application() {
     override fun onCreate() {
         super.onCreate()
         registerActivityLifecycleCallbacks(CurrentActivityProvider)
+        SignUpActivity.camera = object: Camera {
+            override fun call(): Single<String> {
+                return requestPhoto().toSingle()
+            }
+        }
+        SignUpActivity.loginApi = object: LoginApi {
+            override fun checkLogin(login: String): Single<Boolean> {
+                return Single.just(Random().nextBoolean())
+            }
+        }
+        SignUpActivity.system = object : com.elpassion.kt.rx.tdd.workshops.signup.System {
+            override fun cameraPermission(): Single<Boolean> {
+                return requestCameraPermission()
+            }
+        }
+        SignUpActivity.ioScheduler = Schedulers.io()
     }
+
 }
--- app/src/main/java/com/elpassion/kt/rx/tdd/workshops/signup/SignUpActivity.kt
+++ app/src/main/java/com/elpassion/kt/rx/tdd/workshops/signup/SignUpActivity.kt
@@ -1,14 +1,15 @@
 package com.elpassion.kt.rx.tdd.workshops.signup
 
 import android.os.Bundle
+import android.widget.Toast
 import com.elpassion.kt.rx.tdd.workshops.R
 import com.jakewharton.rxbinding2.view.clicks
 import com.jakewharton.rxbinding2.widget.textChanges
 import com.trello.rxlifecycle2.components.RxActivity
 import com.trello.rxlifecycle2.kotlin.bindToLifecycle
 import io.reactivex.Scheduler
-import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.observables.ConnectableObservable
 import kotlinx.android.synthetic.main.sign_up_activity.*
 
 class SignUpActivity : RxActivity() {
@@ -27,19 +28,35 @@ class SignUpActivity : RxActivity() {
                 .mergeWith(clickEvent)
                 .share()
 
-        reducer.invoke(events)
+        val reducerStateEvents = reducer.invoke(events)
                 .observeOn(AndroidSchedulers.mainThread())
                 .bindToLifecycle(this)
-                .subscribe {
-                    loginValidationIndicator.text = it.loginValidation.toString()
-                }
+                .publish()
+
+        reducerStateEvents.loginSth()
+        reducerStateEvents.photoSth()
+
+        reducerStateEvents.connect()
 
     }
+
+    private fun ConnectableObservable<SignUp.State>.loginSth() = map { it.loginValidation }
+            .distinctUntilChanged()
+            .subscribe { loginValidationIndicator.text = it.toString() }
+
+    private fun ConnectableObservable<SignUp.State>.photoSth() = map { it.addPhoto }
+            .distinctUntilChanged()
+            .subscribe {
+                if (it is SignUp.AddPhoto.State.PhotoTaken) {
+                    Toast.makeText(this@SignUpActivity, it.uri, Toast.LENGTH_LONG).show()
+                }
+            }
 
     companion object {
         lateinit var loginApi: LoginApi
         lateinit var ioScheduler: Scheduler
-        lateinit var camera : Camera
-        lateinit var system : System
+        lateinit var camera: Camera
+        lateinit var system: System
     }
+
 }
