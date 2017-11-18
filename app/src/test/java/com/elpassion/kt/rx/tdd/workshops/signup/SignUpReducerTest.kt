6b4323edfa9512618fdd077a4903f9560eaaaef3
Karol Kowalski <ordon1313@gmail.com>
2017-11-18 10:21:21 +0100

    LoginValidationState is in progress after user type login

--- app/src/test/java/com/elpassion/kt/rx/tdd/workshops/signup/SignUpReducerTest.kt
+++ app/src/test/java/com/elpassion/kt/rx/tdd/workshops/signup/SignUpReducerTest.kt
@@ -1,26 +1,47 @@
 package com.elpassion.kt.rx.tdd.workshops.signup
 
 import com.elpassion.kt.rx.tdd.workshops.assertLastValueThat
-import com.elpassion.kt.rx.tdd.workshops.signup.SignUpReducerTest.SignUp.LoginValidation.State
+import com.elpassion.kt.rx.tdd.workshops.common.Events
+import com.elpassion.kt.rx.tdd.workshops.common.Reducer
+import com.elpassion.kt.rx.tdd.workshops.signup.SignUp.*
+import com.jakewharton.rxrelay2.PublishRelay
 import io.reactivex.Observable
 import org.junit.Test
 
 class SignUpReducerTest {
 
-    private val state = Observable.just(SignUp.State(State.IDLE)).test()
+    private val events = PublishRelay.create<Any>()
+    private val state = SignUpReducer().invoke(events).test()
 
     @Test
     fun shouldLoginValidationStateBeIdleOnStart() {
-        state.assertLastValueThat { loginValidation == State.IDLE }
+        state.assertLastValueThat { loginValidation == LoginValidation.State.IDLE }
     }
 
-    interface SignUp {
-        data class State(val loginValidation: LoginValidation.State)
+    @Test
+    fun shouldLoginValidationStateBeInProgressAfterUserTypeLogin() {
+        events.accept(LoginValidation.LoginChangedEvent("a"))
+        state.assertLastValueThat { loginValidation == LoginValidation.State.IN_PROGRESS }
+    }
+}
 
-        interface LoginValidation {
-            enum class State {
-                IDLE,
-            }
+class SignUpReducer : Reducer<SignUp.State> {
+    override fun invoke(events: Events): Observable<SignUp.State> {
+        return events.map { LoginValidation.State.IN_PROGRESS }
+                .startWith(LoginValidation.State.IDLE)
+                .map(SignUp::State)
+    }
+}
+
+interface SignUp {
+    data class State(val loginValidation: LoginValidation.State)
+
+    interface LoginValidation {
+        data class LoginChangedEvent(val login: String)
+
+        enum class State {
+            IDLE,
+            IN_PROGRESS,
         }
     }
 }
